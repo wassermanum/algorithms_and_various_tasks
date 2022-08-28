986692460d536d183b19f62d6c88161a4341f056
Dmitri Chuprin <wassermanum@gmail.com>
2022-08-29 01:20:23 +0600

    push

--- src/main/java/my/learning/Codewars.java
+++ src/main/java/my/learning/Codewars.java
@@ -33,6 +33,28 @@ public class Codewars {
         }
 
         return true;
+    }
 
+    public static int rowSumOddNumbers(int n) {
+        int lastTermInRow = n*n + n - 1;
+        int firstTermInRow = lastTermInRow - 2 * (n - 1);
+        return (firstTermInRow + lastTermInRow) / 2 * n;
+    }
+
+    public static int dontGiveMeFive(int start, int end)
+    {
+        return (int) IntStream.rangeClosed(start, end).boxed().filter(o -> !o.toString().contains("5")).count();
+    }
+
+    public static char findMissingLetter(char[] array)
+    {
+        for (int i = 1; i < array.length; i++) {
+            int currentCharCodepoint = Character.codePointAt(array, i);
+            int previousCharCodepoint = Character.codePointAt(array, i - 1);
+            if (currentCharCodepoint - previousCharCodepoint > 1) {
+                return Character.toString(currentCharCodepoint - 1).charAt(0);
+            }
+        }
+        return ' ';
     }
 }
--- src/main/java/my/learning/Main.java
+++ src/main/java/my/learning/Main.java
@@ -1,12 +1,21 @@
 package my.learning;
 
+import static my.learning.Codewars.*;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println(Codewars.isPrime(7));
-        System.out.println(Codewars.isPrime(4));
-        System.out.println(Codewars.isPrime(13));
-        System.out.println(Codewars.isPrime(7));
-        System.out.println("Hello world!");
+//        System.out.println(Codewars.isPrime(7));
+//        System.out.println(Codewars.isPrime(4));
+//        System.out.println(Codewars.isPrime(13));
+//        System.out.println(Codewars.isPrime(7));
+//        System.out.println("Hello world!");
+//        System.out.println(dontGiveMeFive(3, 8));
+
+        char[] chars = {'a', 'b', 'd'};
+        System.out.println(Character.codePointAt(chars, 0));
+        System.out.println(Character.codePointAt(chars, 1));
+        System.out.println(Character.codePointAt(chars, 2));
+        System.out.println((char) ('a' + 1));
+        System.out.println('d' - 'a');
     }
 }
